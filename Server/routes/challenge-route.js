4995609c53d42569342746692b5d297c5eccc688
masmixto <t.bryndzia@hotmail.com>
2018-02-27 18:58:41 +0100

    List all challenges, add new challenge (protype of update)

--- Server/routes/challenge-route.js
+++ Server/routes/challenge-route.js
@@ -4,6 +4,20 @@ const Challenges = require('../models/challenge-model');
 //const Keys = require('../config/keys');
 const Functions = require('../config/functions');
 
+router.get('/get/:token', Functions.validateRequest, (req,res)=>{
+  Challenges.find({
+    $or: [{
+      author: res.locals.user._id
+    }, {
+      author: 'default'
+    }]
+  }).then(results => {
+    Functions.successfulRes(res, '', results);
+  }).catch(err => {
+    Functions.errorRes(res, [err]);
+  })
+});
+
 router.post('/add', Functions.validateRequest, (req,res)=>{
     let Challenge = new Challenges({
       author: res.locals.user._id,
@@ -14,12 +28,23 @@ router.post('/add', Functions.validateRequest, (req,res)=>{
 
     Challenge.save()
       // Sending callback
-      .then(element => {
-        Functions.successfulRes(res, 'Challenge\' saved', element);
+      .then(challenge => {
+        Functions.successfulRes(res, 'Challenge\' saved', challenge);
       })
       .catch(err => {
         Functions.errorRes(res, [err]);
       });
+});
+
+router.put('/update/:challengeId', Functions.validateRequest, (req,res)=>{
+    Challenges.findOneAndUpdate({_id:req.params.challengeId}, req.body, function(err, challenge) {
+    if (err){
+       Functions.errorRes(res, [err]);
+    }
+    else{
+       Functions.successfulRes(res, 'Challenge\' saved', challenge);
+    }
+  });
 });
 
 module.exports = router;
